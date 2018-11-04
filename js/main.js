a8bc0bf22a6c1cbf712aec4bfc106eb9e1105334
Anifowoshe Gbenga <daveanifowoshe@gmail.com>
2018-11-04 19:29:00 +0100

    Add the get access modal to the app

--- js/main.js
+++ js/main.js
@@ -15,6 +15,37 @@ function toggleMobileSideNav() {
   });
 }
 
+function toggleWhiteListModal() {
+  let openModalBtns = document.querySelectorAll(".get-access-btn");
+  Array.from(openModalBtns).forEach(btn => {
+    btn.addEventListener("click", function(e) {
+      e.preventDefault();
+      document
+        .querySelector(".whitelist-container")
+        .classList.remove("bounceOut");
+      setTimeout(function() {
+        document.querySelector(".whitelist-container").style.zIndex = "100";
+        document
+          .querySelector(".whitelist-container")
+          .classList.add("bounceIn");
+      }, 1);
+    });
+  });
+
+  document
+    .querySelector(".whitelist-close-btn")
+    .addEventListener("click", function(e) {
+      document.querySelector(".whitelist-container").classList.add("bounceOut");
+      setTimeout(function() {
+        document
+          .querySelector(".whitelist-container")
+          .classList.remove("bounceIn");
+        document.querySelector(".whitelist-container").style.zIndex = "-100";
+      }, 1000);
+    });
+}
+
 window.onload = function() {
   toggleMobileSideNav();
+  toggleWhiteListModal();
 };
